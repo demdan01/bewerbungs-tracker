6158f788d838aaad46fd1c20c7e5d375b0936614
demdan01 <[200~145459294+demdan01@users.noreply.github.com~>
2026-02-25 12:35:45 +0100

    chore: add core DOM references (buttons and filters)

---
+++ src/main.js
@@ -0,0 +1,9 @@
+
+
+/*DOM References*/
+const btnNew = document.getElementById("btnNew");
+const searchInput = document.getElementById("searchInput");
+const statusFilter = document.getElementById("statusFilter");
+const btnExportCsv = document.getElementById("btnExportCsv");
+
+console.log(btnNew, searchInput, statusFilter, btnExportCsv);
